408461348ba50074b4dcb2cdb217e48d4c6e0943
cheapo-approved <tvuong@franklinrun.com>
2026-06-29 21:50:25 -0400

    Fixed Random

--- src/app/random/page.tsx
+++ src/app/random/page.tsx
@@ -1,6 +1,8 @@
 import { redirect } from "next/navigation";
 import { getAllStories } from "@/lib/getStory";
 
+export const dynamic = "force-dynamic";
+
 export default function RandomPage() {
   const stories = getAllStories();
   const randomStory = stories[Math.floor(Math.random() * stories.length)];
