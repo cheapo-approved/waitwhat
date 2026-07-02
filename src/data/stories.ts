dca39b84916ccaa80d8ebc2cc7c5ddd509b5969e
cheapo-approved <tvuong@franklinrun.com>
2026-07-02 02:55:25 -0400

    updated body for roman-concrete

--- src/data/stories.ts
+++ src/data/stories.ts
@@ -1,6 +1,6 @@
 import type { Story } from "@/types/story";
 import theStones from "@/data/stories/the-stones";
-//import { romanConcrete } from "@/data/stories/roman-concrete";
+import { romanConcrete } from "@/data/stories/roman-concrete";
 import { gameline } from "@/data/stories/gameline";
 import { antikythera } from "@/data/stories/antikythera";
 import { rcaVideoDisc } from "@/data/stories/rca-video-disc";
@@ -20,7 +20,7 @@ export const stories: Story[] = [
   dancingPlague,
   rcaVideoDisc,
   theStones,
-  //romanConcrete,
+  romanConcrete,
   gameline,
   antikythera,
 ];
